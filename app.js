9eaf9db2a9a8ea10aa7a32f2396472f075bf3002
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-05-29 09:33:17 +0000

    Agregando Extensiones

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var pronoun = ["the", "our"];
 var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
+var extensions = ['.com', '.net', '.us', '.io'];
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let o =0; o < adj.length; o++) {
        for (let p =0; p < noun.length; p++) {
-            console.log(pronoun[i] + adj[o] + noun[p] + ".com"); 
+        for (let l = 0; l < extensions.length; l++)
+            console.log(pronoun[i] + adj[o] + noun[p] + extensions[l]); 
        }
     } 
 }
